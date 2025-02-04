4e77c4761ff9f95f1c125511e980439184d8dcff
darth malu <darth-malu@github.com>
2025-02-04 04:52:24 +0300

    +

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "abstracT nixConfig";
   inputs = {
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github.com/NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     # nixpkgs.url = "nixpkgs/nixos-24.11" || nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     yazi.url = "github:sxyazi/yazi";

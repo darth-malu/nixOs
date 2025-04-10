0245e90288db29dee03bba27cc4cbcdff8b6990c
darth malu <darth-malu@github.com>
2025-04-10 22:54:16 +0300

    hyprpaper tangier/carthage

--- modules/home/hyprland/hyprland-helper/hypr-ecos/hyprpaper.nix
+++ modules/home/hyprland/hyprland-helper/hypr-ecos/hyprpaper.nix
@@ -1,38 +1,44 @@
-{ inputs, pkgs, osConfig, lib, ... }:
+{
+  inputs,
+  pkgs,
+  osConfig,
+  lib,
+  ...
+}:
 
-let #FIXME this block is not working
+let # FIXME this block is not working
   # requirements:
   # - output alist [
   #   monitor1, location
   #   monitor2, location
   # ]
-  set_wallpaper_script = pkgs.writeShellScriptBin "set_wallpaper.sh" ''
-  # declare -a monitors
-    if [[ "$HOSTNAME" == "carthage" ]]; then
-      monitor1_wallpaper="/home/malu/Pictures/wallpapers/singed.png"
-      monitor2_wallpaper="/home/malu/Pictures/wallpapers/thresh_purp.png"
+  set_wallpaper = pkgs.writeShellScriptBin "set_wallpaper.sh" ''
+    # declare -a monitors
+      if [[ "$HOSTNAME" == "carthage" ]]; then
+        monitor1_wallpaper="/home/malu/Pictures/wallpapers/singed.png"
+        monitor2_wallpaper="/home/malu/Pictures/wallpapers/thresh_purp.png"
 
-      while IFS= read -r line; do
-        monitor_id=$(echo "$line" | cut -d ' ' -f 2)
-        if [[ "$line" == *"HDMI-A-1"* ]]; then
-          # echo "$monitor1_id,$monitor1_wallpaper"
-          # monitors+=("$monitor_id,$monitor1_wallpaper")
-          hyprctl hyprpaper wallpaper "HDMI-A-1,$HOME/Pictures/wallpapers/singed.png"
-        elif [[ "$line" == *"DP"* ]]; then
-          # monitors+=("$monitor_id,$monitor2_wallpaper")
-          # echo "$monitor2_id,$monitor2_wallpaper"
-          hyprctl hyprpaper wallpaper "$monitor_id,$HOME/Pictures/wallpapers/thresh_purp.png"
-        fi
-      done < <(hyprctl monitors)
+        while IFS= read -r line; do
+          monitor_id=$(echo "$line" | cut -d ' ' -f 2)
+          if [[ "$line" == *"HDMI-A-1"* ]]; then
+            # echo "$monitor1_id,$monitor1_wallpaper"
+            # monitors+=("$monitor_id,$monitor1_wallpaper")
+            hyprctl hyprpaper wallpaper "HDMI-A-1,$HOME/Pictures/wallpapers/singed.png"
+          elif [[ "$line" == *"DP"* ]]; then
+            # monitors+=("$monitor_id,$monitor2_wallpaper")
+            # echo "$monitor2_id,$monitor2_wallpaper"
+            hyprctl hyprpaper wallpaper "$monitor_id,$HOME/Pictures/wallpapers/thresh_purp.png"
+          fi
+        done < <(hyprctl monitors)
 
-      # Output the array elements separated by newlines (without brackets)
-      # for i in "''${monitors[@]}"; do
-      #   printf '"%s"\n' "$i"
-      # done
+        # Output the array elements separated by newlines (without brackets)
+        # for i in "''${monitors[@]}"; do
+        #   printf '"%s"\n' "$i"
+        # done
 
-    elif [[ "$HOSTNAME" == "tangier" ]]; then
-      echo "eDP-1,$HOME/Pictures/wallpapers/singed.png" # Use $HOME for portability
-    fi
+      elif [[ "$HOSTNAME" == "tangier" ]]; then
+        echo "eDP-1,$HOME/Pictures/wallpapers/singed.png" # Use $HOME for portability
+      fi
   '';
   # wall = "${set_wallpaper_script}/bin/set_wallpaper.sh"; TODO see if its possible to do this
 in
@@ -45,16 +51,28 @@ in
     settings = {
       ipc = true;
       splash = true;
-      preload = [
-        "$HOME/Pictures/wallpapers/singed-4096x2305.jpg"
-        "$HOME/Pictures/wallpapers/jinx.png"
-        "$HOME/Pictures/wallpapers/thresh_purp.png"
-      ];
+      preload =
+        if osConfig.networking.hostName == "carthage" then
+          [
+            "$HOME/Pictures/wallpapers/singed-4096x2305.jpg"
+            "$HOME/Pictures/wallpapers/jinx.png"
+            "$HOME/Pictures/wallpapers/thresh_purp.png"
+          ]
+        else if osConfig.networking.hostname == "tangier" then
+          [ "$HOME/Pictures/wallpapers/vi_xander.png" ]
+        else
+          [ ];
       # wallpaper = wall;
-      wallpaper = [
-        "HDMI-A-1,$HOME/Pictures/wallpapers/jinx.png"
-        "DP-3,$HOME/Pictures/wallpapers/singed-4096x2305.jpg"
-      ];
+      wallpaper =
+        if osConfig.networking.hostName == "carthage" then
+          [
+            "HDMI-A-1,$HOME/Pictures/wallpapers/jinx.png"
+            "DP-3,$HOME/Pictures/wallpapers/singed-4096x2305.jpg"
+          ]
+        else if osConfig.networking.hostname == "tangier" then
+          [ "$HOME/Pictures/wallpapers/vi_xander.png" ]
+        else
+          [ ];
     };
   };
 }

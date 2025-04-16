249fb8bb2028362e21e4c9e5e0a435a3abf41fb8
darth malu <darth-malu@github.com>
2025-04-16 23:05:44 +0300

    made mpris script THIN

--- modules/home/hyprland/hyprland-helper/scripts/mpris_volume_dunst.nix
+++ modules/home/hyprland/hyprland-helper/scripts/mpris_volume_dunst.nix
@@ -4,57 +4,38 @@ pkgs.writeShellScriptBin "mpris_vol" ''
   msgTag="mpris_volume"
 
   convert_to_percentage() {
-      # local value=$1
-      # local percentage
-      #
-      # percentage=$(echo "scale=2; $value * 100 / 1" | bc)
-      # printf "%.0f" "$percentage" # returns % eg 90
-      # echo "scale=2; $1 * 100" | bc | xargs printf '%.0f'
-      # printf "%.0f%" "echo $(scale=2 $1 * 100 | bc)"
-      printf "%.0f" "echo $(scale=2 $1 * 100 | bc)"
+    printf "%.0f" "$(bc <<< "scale=2; $1 * 100")" # printf "%.0f" "$(echo "scale=2; $1 * 100" | bc)"
   }
 
-  playerctl_add_sub() {
-      playerctl -p "$1" volume 0.02"$2"
-  }
-
-  mpc_add_sub() {
-      mpc volume "$1"2 >/dev/null
-  }
-
-  # query_playerctl() {
-  #     playerctl -l
-  # }
-
-  # current_volume() {
-  # convert_to_percentage "$(playerctl -p "$1" volume)" # convert %0.2f (eg. 0.54000) into percentage out of 1
-  # specific player not needed since it will auto pick current one
-  # }
+  get_volume() {
+    convert_to_percentage "$(playerctl volume)"
+  } # remove need for local volume in every case block
 
   player_volume() {
       local current_player=$(playerctl metadata --format '{{ playerName }}')
       # local volume=$(current_volume "$current_player")
-      local volume=$(convert_to_percentage$(playerctl volume))
-
-      # local add_minus=$1 # intakes + or -
+      # local volume
 
       case $current_player in
       "spotify")
           # volume=$(current_volume "$current_player")
+          # needs to be absolute path for dunstify to work
           playerctl -p "$current_player" volume 0.02"$1"
+          # volume=$(convert_to_percentage "$(playerctl volume)")
           dunstify -t 1000 -a "changeVolume" -u low \
-            -i ../../../../assets/icons/icons8-spotify-gradient/icons8-spotify-50.png \
-            -h string:x-dunst-stack-tag:$msgTag "Spotify                  $volume   " \
-            -h int:value:"$volume"
+            -i $HOME/Shibuya/assets/icons/icons8-spotify-gradient/icons8-spotify-50.png \
+            -h int:value:"$(get_volume)"
+            # -h string:x-dunst-stack-tag:$msgTag "Spotify            $(get_volume)" \
           ;;
       "Lollypop")
           # playerctl_add_sub "$current_player" "$add_minus"
           # volume=$(current_volume "$current_player")
           playerctl -p "$current_player" volume 0.02"$1"
+          volume=$(convert_to_percentage "$(playerctl volume)")
           dunstify -t 1000 -a "changeVolume" -u low \
-            -i ../../../../assets/icons/icons8-spotify-gradient/icons8-spotify-50.png \
-            -h string:x-dunst-stack-tag:$msgTag "Lollypop                  $volume" \
-            -h int:value:"$volume"
+            -i $HOME/Shibuya/assets/icons/icons8-candy-50.png \
+            -h int:value:"$(get_volume)"
+            # -h string:x-dunst-stack-tag:$msgTag "Lollypop                  $(get_volume)" \
           ;;
       "mpd")
           mpc volume "$1"1 && songart "ncmpcpp_volume"
@@ -63,19 +44,21 @@ pkgs.writeShellScriptBin "mpris_vol" ''
       "mpv")
           # playerctl_add_sub "$current_player" "$add_minus"
           playerctl -p "$current_player" volume 0.02"$1"
+          volume=$(convert_to_percentage "$(playerctl volume)")
           dunstify -t 1000 -a "changeVolume" -u low \
-            -i ../../../../assets/icons/icons8-spotify-gradient/icons8-spotify-50.png \
-            -h string:x-dunst-stack-tag:$msgTag "MPV                  $volume" \
-            -h int:value:"$volume"
+            -i $HOME/Shibuya/assets/icons/mpv.256x253.png \
+            -h int:value:"$(get_volume)"
+            # -h string:x-dunst-stack-tag:$msgTag "MPV                  $(get_volume) " \
           ;;
       "chromium"*)
           # playerctl_add_sub "$current_player" "$add_minus"
           # volume=$(current_volume "$current_player")
           playerctl -p "$current_player" volume 0.02"$1"
+          volume=$(convert_to_percentage "$(playerctl volume)")
           dunstify -t 1000 -a "changeVolume" -u low \
-            -i ../../../../assets/icons/icons8-spotify-gradient/icons8-spotify-50.png \
-            -h string:x-dunst-stack-tag:$msgTag "Chrome                  $volume" \
-            -h int:value:"$volume"
+            -i $HOME/Shibuya/assets/icons/icons8-chrome-office-l/icons8-chrome-30.png \
+            -h int:value:"$(get_volume)"
+            # -h string:x-dunst-stack-tag:$msgTag "Chrome                  $(get_volume)" \
           ;;
       *)
           playerctl -p "$current_player" volume 0.02"$1"

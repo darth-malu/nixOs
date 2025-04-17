9a7f5b70f55e7c80e4f76fbfc1d8594f2aad34a3
darth malu <darth-malu@github.com>
2025-04-17 21:45:52 +0300

    ++

--- modules/nix/sync-thing/default.nix
+++ modules/nix/sync-thing/default.nix
@@ -36,43 +36,43 @@ folders = {
     devices = [ "tangier" "carthage" ];      # Which devices to share the folder with
   };
 
-  "Taskwarrior" = {
-    path = "/home/malu/.task";
-    devices = [
-      "tangier"
-      "carthage"
-    ];
-    type = "sendreceive";
-  };
+"Taskwarrior" = {
+  path = "/home/malu/.task";
+  devices = [
+    "tangier"
+    "carthage"
+  ];
+  type = "sendreceive";
+};
 
-  "ytVid" = {
-    path = "/home/malu/Videos/YtDlp";
-    devices = [ "carthage" ]; # device to receive from?
-    type = "receiveonly"; # sendreceive::,sendonly, receiveonly, receiveencrypted
-    order = "newestFirst"; # random(default), alphabetic, smallestFirst, largestFirst,
-    # maxConflicts = 2; # -1:: unlimited number , 0 - disables
-    maxConcurrentWrites = 4; # 2:: Maximum number of concurrent write operations while syncing. Increasing this might increase or decrease disk performance, depending on the underlying storage.
-    # scanProgressIntervalS = 0; # The interval in seconds with which scan progress information is sent to the GUI. Setting to 0 will cause Syncthing to use the default value of two.
-    minHomeDiskFree = {
-      # % (percent of the disk / volume size), kB, MB, GB and TB.
-      unit = "%";
-      value = 15;
-    };
+"ytVid" = {
+  path = "/home/malu/Videos/YtDlp";
+  devices = [ "carthage" ]; # device to receive from?
+  type = "receiveonly"; # sendreceive::,sendonly, receiveonly, receiveencrypted
+  order = "newestFirst"; # random(default), alphabetic, smallestFirst, largestFirst,
+  # maxConflicts = 2; # -1:: unlimited number , 0 - disables
+  maxConcurrentWrites = 4; # 2:: Maximum number of concurrent write operations while syncing. Increasing this might increase or decrease disk performance, depending on the underlying storage.
+  # scanProgressIntervalS = 0; # The interval in seconds with which scan progress information is sent to the GUI. Setting to 0 will cause Syncthing to use the default value of two.
+  minHomeDiskFree = {
+    # % (percent of the disk / volume size), kB, MB, GB and TB.
+    unit = "%";
+    value = 15;
   };
+};
 
-  "music" = {
-    path = "/home/malu/Music/homeMusic";
-    devices = [
-      "tangier"
-      "carthage"
-    ];
-    type = "sendreceive";
-    minHomeDiskFree = {
-      # % (percent of the disk / volume size), kB, MB, GB and TB.
-      unit = "%";
-      value = 40;
-    };
+"music" = {
+  path = "/home/malu/Music/homeMusic";
+  devices = [
+    "tangier"
+    "carthage"
+  ];
+  type = "sendreceive";
+  minHomeDiskFree = {
+    # % (percent of the disk / volume size), kB, MB, GB and TB.
+    unit = "%";
+    value = 40;
   };
+};
 
   "myVaults" = {
     path = "/home/malu/obsidianVaults";

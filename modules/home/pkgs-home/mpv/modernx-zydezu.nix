a4f87e06b114c9539e611ab48fbb7ed9497b3659
darth malu <darth-malu@github.com>
2025-04-30 13:35:42 +0300

    quteBrowser , modernx-zydezu

---
+++ modules/home/pkgs-home/mpv/modernx-zydezu.nix
@@ -0,0 +1,11 @@
+{
+    compact_mode=no
+    info_button=yes
+    title_font_size=20
+    seekbar_handle_size=0
+    jump_buttons = false;                    -- show the jump backward and forward buttons
+    persistent_progress = false;            -- always show a small progress line at the bottom of the screen
+    persistent_progressheight = 17;         -- the height of the persistent_progress bar
+    persistent_buffer = false;              -- on web videos, show the buffer on the persistent progress line
+    persistent_progresstoggle = true;       -- enable toggling the persistent_progress bar
+}

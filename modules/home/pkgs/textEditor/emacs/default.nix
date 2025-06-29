b6524050c992be2a2a625fe9f566c3cb97ad6a71
darth-malu <darth-malu@github.com>
2025-06-29 18:59:33 +0300

    try classic emacs tangier pgtk carrghatge

--- modules/home/pkgs/textEditor/emacs/default.nix
+++ modules/home/pkgs/textEditor/emacs/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   imports = [
     ./emacsPkgs.nix
@@ -7,7 +7,7 @@
   config = {
     programs.emacs = {
       enable = true;
-      package = pkgs.emacs-pgtk; # emacs, emacs-gtk, emacs-nox, emacs-pgtk
+      package = if config.networking.hostName == "tangier" then pkgs.emacs else pkgs.emacs-pgtk; # emacs, emacs-gtk, emacs-nox, emacs-pgtk
       # extraConfig = ''
       #   (setq standard-indent 2)
       # ''; # init.el

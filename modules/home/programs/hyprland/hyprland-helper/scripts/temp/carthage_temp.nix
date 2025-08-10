8b5f8d814eb9d8d62c7bcc0038d41579d17e10f3
darth-malu <darth-malu@github.com>
2025-08-10 04:47:42 +0300

    awk simplification 😄

--- modules/home/programs/hyprland/hyprland-helper/scripts/temp/carthage_temp.nix
+++ modules/home/programs/hyprland/hyprland-helper/scripts/temp/carthage_temp.nix
@@ -3,17 +3,14 @@ pkgs.writeShellScriptBin "temp" ''
 
   gpu_temp() {
     # Returns temperature in millidegrees Celsius (multiplied by 1000).
-    # local raw_temp="$(cat /sys/class/drm/card1/device/hwmon/hwmon2/temp1_input 2> /dev/null)"
-    # echo -n "$(($raw_temp / 1000))"
-    printf '%d' "$(($(cat /sys/class/drm/card1/device/hwmon/hwmon2/temp1_input 2> /dev/null) / 1000))"
+    awk '{print int($0/1000)}' /sys/class/drm/card*/device/hwmon/hwmon*/temp1_input | head -n1 #  only print the first match
   }
 
   gpu_fan() {
-      # returns number eg 700 - rpm
-      cat /sys/class/drm/card1/device/hwmon/hwmon0/fan1_input 2> /dev/null
+      cat /sys/class/drm/card1/device/hwmon/hwmon0/fan1_input # returns number eg 700 - rpm
   }
 
-  gpu_busy() {
+  gpu_busy() {                  # %
       cat /sys/class/drm/card1/device/gpu_busy_percent 2> /dev/null
   }
 
@@ -21,7 +18,8 @@ pkgs.writeShellScriptBin "temp" ''
       #outputs [:digit:]Mhz eg 600Mhz
       # list with current *
       #cat /sys/class/drm/card1/device/pp_dpm_mclk | grep "*"
-      grep -oE "\s+[0-9]*.*\*" </sys/class/drm/card1/device/pp_dpm_sclk | tr -cd "[:alnum:]" 2> /dev/null
+      # grep -oE "\s+[0-9]*.*\*" </sys/class/drm/card1/device/pp_dpm_sclk | tr -cd "[:alnum:]" 2> /dev/null
+      awk '/*/ {print $2}' /sys/class/drm/card1/device/pp_dpm_sclk
   }
 
   nvme_temp() {
@@ -29,11 +27,14 @@ pkgs.writeShellScriptBin "temp" ''
       local nvme_temp="$(cat /sys/class/hwmon/hwmon1/temp1_input)" #get temp in mC
       local temp=$(echo "scale=2; $nvme_temp / 1000" | bc)         # convert to C
       printf "%.0f\n" "$temp"                                # temp float - int
+
+      awk '{print int($0/1000)}' /sys/class/hwmon/hwmon1/temp1_input
   }
 
   ssd_temp() {
-      local ssd_temp="$(cat /sys/class/hwmon/hwmon1/temp1_input 2>/dev/null)" #get temp in mC
-      printf "%.0f\n" "$(echo "scale=2; $(cat /sys/class/hwmon/hwmon1/temp1_input 2>/dev/null) / 1000" | bc)"                                           # temp float - int
+      # local ssd_temp="$(cat /sys/class/hwmon/hwmon1/temp1_input 2>/dev/null)" #get temp in mC
+      # printf "%.0f\n" "$(echo "scale=2; $(cat /sys/class/hwmon/hwmon1/temp1_input 2>/dev/null) / 1000" | bc)"                                           # temp float - int
+      awk '{ print int($0/1000)}' /sys/class/hwmon/hwmon1/temp1_input
   }
 
   proc_cpu() {

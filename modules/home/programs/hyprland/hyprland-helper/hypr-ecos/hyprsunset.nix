2ef6533c00189e0a2e13070f56f10cb44fe7ac69
darth-malu <darth-malu@github.com>
2025-08-11 15:01:07 +0300

    ++

--- modules/home/programs/hyprland/hyprland-helper/hypr-ecos/hyprsunset.nix
+++ modules/home/programs/hyprland/hyprland-helper/hypr-ecos/hyprsunset.nix
@@ -2,28 +2,27 @@
 {
   services.hyprsunset = {
     enable = true;
-    # package = inputs.hyprsunset.packages.${pkgs.system}.hyprsunset;
     extraArgs = [ "--identity" ];
-    # transitions = {
-    #   sunrise = {
-    #     calendar = "*-*-* 06:00:00"; # TODO systemd time?
-    #     requests = [
-    #       [
-    #         "temperature"
-    #         "6500"
-    #       ]
-    #       # [ "gamma 100" ]       # like full brightness
-    #     ];
-    #   };
-    #   sunset = {
-    #     calendar = "*-*-* 19:00:00";
-    #     requests = [
-    #       [
-    #         "temperature"
-    #         "3500"
-    #       ]
-    #     ];
-    #   };
-    # };
+    transitions = {
+      sunrise = {
+        calendar = "*-*-* 06:00:00"; # TODO systemd time?
+        requests = [
+          [
+            "temperature"
+            "6500"
+          ]
+          # [ "gamma 100" ]       # like full brightness
+        ];
+      };
+      sunset = {
+        calendar = "*-*-* 19:00:00";
+        requests = [
+          [
+            "temperature"
+            "3500"
+          ]
+        ];
+      };
+    };
   };
 }

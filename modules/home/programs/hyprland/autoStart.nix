a8ec798e73f55f10f60302505bd76b80dd71abf6
darth-malu <darth-malu@github.com>
2025-08-27 02:51:29 +0300

    qs + hyprpolkit

--- modules/home/programs/hyprland/autoStart.nix
+++ modules/home/programs/hyprland/autoStart.nix
@@ -1,14 +1,13 @@
 {
   wayland.windowManager.hyprland.settings.exec-once = [
     # "systemctl --user enable --now hyprpolkitagent.service"
-    # "/nix/store/*hyprpolkitagent*/libexec/hyprpolkitagent &"
-    "uwsm app -s b /nix/store/*hyprpolkitagent*/libexec/hyprpolkitagent"
+    "/nix/store/*hyprpolkitagent*/libexec/hyprpolkitagent &"
     # "uwsm app -s b ${pkgs}/libexec/hyprpolkitagent"
     # "systemctl --user enable --now hyprsunset.service" #TODO write a script to auto do this
     "uwsm app -s b sway-audio-idle-inhibit"
-    "uwsm app -s b quickshell"
     # "uwsm app -s b waybar"
     "[workspace special:easy silent] uwsm app -s a easyeffects"
+    # "uwsm app -s a qbittorent" # FIXME not work
     "wl-paste --type image --watch cliphist store"
     "wl-paste --type text --watch cliphist store"
   ];

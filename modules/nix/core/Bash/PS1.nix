427d8ba79fbd5355065f457fd4b216fbffe8fd59
darth-malu <darth-malu@github.com>
2025-11-23 17:10:29 +0300

    Back to old ps1

--- modules/nix/core/Bash/PS1.nix
+++ modules/nix/core/Bash/PS1.nix
@@ -1,17 +1,15 @@
 ''
-  exitstatus() {
+exitstatus() {
     local stat="$?"
     if [[ $stat -ne 0 ]]; then
-      printf "%s" "$(tput setaf 199)" # 161
+        printf "%s" "$(tput setaf 199)" # 161
     else
-      printf "%s" "$(tput setaf 43)" # 43
+        printf "%s" "$(tput setaf 43)" # 43
     fi
-  }
+}
 
-  EXIT_COLOR="\[\$(exitstatus)\]"
-  BOLD="\[$(tput bold)\]"
-  RESET="\[$(tput sgr0)\]"
-
-  PS1="\n$EXIT_COLOR$BOLD \w $RESET\n "
-  # TODO ... maybe need git? lol
+EXIT_COLOR="\[\$(exitstatus)\]"
+BOLD="\[$(tput bold)\]"
+RESET="\[$(tput sgr0)\]"
+PS1="\n$EXIT_COLOR$BOLD \w $RESET"
 ''

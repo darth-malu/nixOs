47ff079dc1e76ca8a3f6b6ce83c82f5464eb2267
darth-malu <darth-malu@github.com>
2025-12-16 02:50:58 +0300

    Top, Btop tui

--- modules/home/programs/shell/terminal/kitty/tui.nix
+++ modules/home/programs/shell/terminal/kitty/tui.nix
@@ -1,13 +1,13 @@
 {
   programs.kitty.keybindings = {
-    # Custom tui launches
-    f9 = "launch_tab --title yazi-spawn yazi";
+    f1 = "launch_tab btop";
     f10 = "launch_tab nyaa";
     f12 = "launch_tab ncmpcpp";
-    # "f1" = "launch_tab btop";
+    f2 = "launch_tab top";
+    f9 = "launch_tab --title yazi-spawn yazi";
     # "SUPER + f1" = "launch_tab btop";
     f11 = "launch_window --location vsplit ncmpcpp";
-    # f4 = "launch --stdin-source=@screen_scrollback --stdin-add-formatting --type=overlay less +G -R";
+    f4 = "launch --stdin-source=@screen_scrollback --stdin-add-formatting --type=overlay less +G -R";
     # "f5" = "new_window_with_cwd";
   };
 }

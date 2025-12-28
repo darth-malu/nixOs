67104738ad065a82cae05d6ffe5f885105264cb7
darth-malu <darth-malu@github.com>
2025-12-28 03:22:09 +0300

    nmcpp sane + extras

--- modules/home/programs/soundStuff/ncmpcpp/default.nix
+++ modules/home/programs/soundStuff/ncmpcpp/default.nix
@@ -5,237 +5,264 @@
 
     mpdMusicDir = "~/Music";
 
-  bindings = [
-  # scroll
-  { key = "j";command = "scroll_down";}
+    bindings = [
+      # scroll
+      {
+        key = "j";
+        command = "scroll_down";
+      }
 
-  { key = "k"; command = "scroll_up"; }
+      {
+        key = "k";
+        command = "scroll_up";
+      }
 
-  {key = "l"; command = "next_column";}
+      {
+        key = "l";
+        command = "next_column";
+      }
 
-  # {key = "L";command = "next_screen";}
-  # {key = "H";command = "master_screen";}
+      # {key = "L";command = "next_screen";}
+      # {key = "H";command = "master_screen";}
 
-  # { key = "H"; command = "previous_screen"; }
-  # { key = "L"; command = "slave_screen"; }
+      # { key = "H"; command = "previous_screen"; }
+      # { key = "L"; command = "slave_screen"; }
 
-  { key = "h"; command = "previous_column"; }
+      {
+        key = "h";
+        command = "previous_column";
+      }
 
-  # { key = "shift-tab"; command = "previous"; }
+      # { key = "shift-tab"; command = "previous"; }
 
-  # { key = "tab"; command = "next"; }
+      # { key = "tab"; command = "next"; }
 
-  ##lyrics
-  { key = "'";  command = "show_lyrics"; }
+      ##lyrics
+      {
+        key = "'";
+        command = "show_lyrics";
+      }
 
-  { key = "^"; command = "toggle_lyrics_fetcher"; }
+      {
+        key = "^";
+        command = "toggle_lyrics_fetcher";
+      }
 
-  # playlists manipulation
-  { key = "ctrl-j"; command = [ "select_item" "scroll_down" ]; }
+      # playlists manipulation
+      {
+        key = "ctrl-j";
+        command = [
+          "select_item"
+          "scroll_down"
+        ];
+      }
 
-  # { key = "ctrl-v"; command = "select_range"; }
+      # { key = "ctrl-v"; command = "select_range"; }
 
-  # WARN: DANGEROUS
-  { key = "d"; command = "delete_browser_items"; }
+      # WARN: DANGEROUS
+      {
+        key = "d";
+        command = "delete_browser_items";
+      }
 
-  {
-    key = "ctrl-k";
-    command = [
-      "select_item"
-      "scroll_up"
+      {
+        key = "ctrl-k";
+        command = [
+          "select_item"
+          "scroll_up"
+        ];
+      }
+
+      {
+        key = "J";
+        command = [
+          "move_end"
+        ];
+      }
+
+      {
+        key = "K";
+        command = [
+          "move_home"
+        ];
+      }
+
+      {
+        key = "0";
+        command = "run_external_command  \"songart\"";
+      }
+
     ];
-  }
 
-  {
-    key = "J";
-    command = [
-      "move_end"
-    ];
-  }
+    settings = {
 
-  {
-    key = "K";
-    command = [
-      "move_home"
-    ];
-  }
+      startup_screen = "playlist";
+      user_interface = "classic"; # classic , alternative #TODO: add snippet for nice comments
+      default_place_to_search_in = "database"; # playlist
 
-  {
-    key = "0";
-    command = "run_external_command  \"songart\"";
-  }
+      message_delay_time = 2;
+      connected_message_on_startup = false;
 
-  ];
+      locked_screen_width_part = "50"; # 20-80::
+      ask_for_locked_screen_width_part = true; # disable to use default
 
-settings = {
+      jump_to_now_playing_song_at_start = true;
+      #now_playing_prefix = " "; # 󱉺  
+      now_playing_prefix = "  "; # TODO:check how to filter out songs from playlist
+      now_playing_suffix = "  ";
 
-startup_screen = "playlist";
-user_interface = "classic"; # classic , alternative #TODO: add snippet for nice comments
-default_place_to_search_in = "database"; #playlist
+      external_editor = "/home/malu/.nix-profile/bin/emacsclient -nc";
+      use_console_editor = true; # TODO: what is sit doing?
 
-message_delay_time = 2;
-connected_message_on_startup = false;
+      # functionality
+      centered_cursor = true; # playing focus
+      autocenter_mode = true; # Centers highlight on current song during switch
 
-locked_screen_width_part = "50"; # 20-80::
-ask_for_locked_screen_width_part = true; # disable to use default
+      block_search_constraints_change_if_items_found = true; # fields in Search engine above "Reset" will be blocked after successful searching
 
-jump_to_now_playing_song_at_start = true;
-#now_playing_prefix = " "; # 󱉺  
-now_playing_prefix = "  ";#TODO:check how to filter out songs from playlist
-now_playing_suffix = "  ";
+      cyclic_scrolling = true;
 
-external_editor = "/home/malu/.nix-profile/bin/emacsclient -nc";
-use_console_editor = true;#TODO: what is sit doing?
+      display_bitrate = false;
 
-# functionality
-centered_cursor = true; #playing focus
-autocenter_mode = true; # Centers highlight on current song during switch
+      enable_window_title = true;
 
-block_search_constraints_change_if_items_found = true; # fields in Search engine above "Reset" will be blocked after successful searching
+      ignore_diacritics = true; # local language ^ pronounciation marker eg. shadda, umlauts TODO : investigate if this is what is causing issues with -
 
-cyclic_scrolling = true;
+      ignore_leading_the = true;
 
-display_bitrate = false;
+      tag_editor_extended_numeration = true;
+      empty_tag_marker = "*empty*";
+      #empty_tag_color = "";
 
-enable_window_title = true;
+      media_library_primary_tag = "album_artist"; # album_artist, artist, date, genre, composer, performer
+      media_library_albums_split_by_date = true;
+      media_library_hide_album_dates = true;
+      # media_library_disable_two_column_mode = false; # false::
 
-ignore_diacritics = true; # local language ^ pronounciation marker eg. shadda, umlauts TODO : investigate if this is what is causing issues with -
+      default_find_mode = "wrapped"; # wrapped - go to match as you type, normal <key>w
 
-ignore_leading_the = true;
+      lyrics_directory = "~/Music/ncmpcpp/lyrics";
+      store_lyrics_in_song_dir = false;
+      fetch_lyrics_for_current_song_in_background = false;
 
-tag_editor_extended_numeration = true;
-empty_tag_marker = "*empty*";
-#empty_tag_color = "";
+      # lyrics_fetchers = "genius , azlyrics";
+      lyrics_fetchers = "genius";
+      # lyrics
+      follow_now_playing_lyrics = true;
+      # lyrics_directory = "~/.local/share/darth_ncmpcpp";
 
-media_library_primary_tag = "album_artist"; #album_artist, artist, date, genre, composer, performer
-media_library_albums_split_by_date = true;
-media_library_hide_album_dates = true;
-# media_library_disable_two_column_mode = false; # false::
+      incremental_seeking = "yes";
+      seek_time = "1";
 
-default_find_mode = "wrapped"; #wrapped - go to match as you type, normal <key>w
+      titles_visibility = false;
 
-lyrics_directory = "~/Music/ncmpcpp/lyrics";
-store_lyrics_in_song_dir = false;
-fetch_lyrics_for_current_song_in_background = false;
+      header_text_scrolling = true;
+      header_visibility = true; # If enabled, header window will be displayed, otherwise hidden.
+      header_window_color = 136; # page title #holy purplee
 
-# lyrics_fetchers = "genius , azlyrics";
-lyrics_fetchers = "genius";
-# lyrics
-follow_now_playing_lyrics = true;
-# lyrics_directory = "~/.local/share/darth_ncmpcpp";
+      color1 = 112;
+      color2 = 136;
 
-incremental_seeking = "yes";
-seek_time = "1";
+      # song_columns_list_format = "(40)[39]{t} | (30)[39]{a} | (30)[39]{f} (25)[39]{b} (5)[39]{l}"; # clean
+      song_columns_list_format = "(40)[39]{t} (30)[39]{a} (25)[39]{b} (5)[39]{l}"; # alternatives dont work in column list
+      #nice : 38
 
-titles_visibility = false;
+      song_list_format = "{$5%t} | {$6%f$9} $r %p %b      {$4%a$1$9}$9";
 
-header_text_scrolling = true;
-header_visibility = true; # If enabled, header window will be displayed, otherwise hidden.
-header_window_color = 136; # page title #holy purplee
+      current_item_prefix = "$(142)$r"; # highlight current
 
-color1 = 112;
-color2 = 136;
+      current_item_inactive_column_prefix = "$(24)$r"; # nice purp
 
-# song_columns_list_format = "(40)[39]{t} | (30)[39]{a} | (30)[39]{f} (25)[39]{b} (5)[39]{l}"; # clean
-song_columns_list_format = "(40)[39]{t} (30)[39]{a} (25)[39]{b} (5)[39]{l}"; # alternatives dont work in column list
-#nice : 38
+      alternative_header_first_line_format = "$0$aqqu$/a$9  {$6%a$9     }{$5%t$9}|{$3%f$9}  $0$atqq$/a$9";
+      alternative_header_second_line_format = "{{$7%b$9}}|{$4%d$9}";
+      alternative_ui_separator_color = 1;
 
-song_list_format = "{$5%t} | {$6%f$9} $r %p %b      {$4%a$1$9}$9";
+      window_border_color = 212; # pop up windows
+      active_window_border = 21;
 
-current_item_prefix = "$(142)$r"; # highlight current
+      state_line_color = 1;
+      state_flags_color = 4; # mpc status flags eg rzxu
 
-current_item_inactive_column_prefix = "$(24)$r"; #nice purp
+      main_window_color = 69;
 
-alternative_header_first_line_format = "$0$aqqu$/a$9  {$6%a$9     }{$5%t$9}|{$3%f$9}  $0$atqq$/a$9";
-alternative_header_second_line_format = "{{$7%b$9}}|{$4%d$9}";
-alternative_ui_separator_color = 1;
+      player_state_color = 87; # playing/paused status
 
-window_border_color = 212; #pop up windows
-active_window_border = 21;
+      # [[file:default.org::*status bar][status bar:1]]
+      statusbar_time_color = 87;
+      statusbar_color = 69; # 227 song name on status bar
 
-state_line_color = 1;
-state_flags_color = 4; # mpc status flags eg rzxu
+      statusbar_visibility = true;
+      # status bar:1 ends here
 
-main_window_color = 69;
+      display_remaining_time = false; # instead of elapsed in status bar
 
-player_state_color = 87; # playing/paused status
+      volume_color = 124;
+      display_volume_level = true;
+      volume_change_step = 2;
 
-# [[file:default.org::*status bar][status bar:1]]
-statusbar_time_color = 87;
-statusbar_color = 69; # 227 song name on status bar
+      progressbar_color = 24;
+      # progressbar_look = "─⊙┄";
+      progressbar_look = "─⊙ ";
+      #progressbar_look = "─░─";
+      progressbar_elapsed_color = 74;
+      #progressbar_elapsed_color = 24; # muted
 
-statusbar_visibility = true;
-# status bar:1 ends here
+      colors_enabled = true;
 
-display_remaining_time = false; #instead of elapsed in status bar
+      playlist_disable_highlight_delay = 5; # 0 to disable fade
+      playlist_show_remaining_time = true; # header time remaining
+      playlist_shorten_total_times = true;
+      playlist_separate_albums = false; # add separator lines between albums in playlist view
+      ask_before_clearing_playlists = true;
 
-volume_color = 124;
-display_volume_level = true;
-volume_change_step = 2;
+      search_engine_display_mode = "columns";
+      browser_display_mode = "columns";
+      playlist_display_mode = "columns";
+      playlist_editor_display_mode = "columns";
+      # browser_playlist_prefix = " - ";
+      # browser_playlist_prefix = " -> ";
+      #clock_display_seconds = yes
 
-progressbar_color = 24;
-# progressbar_look = "─⊙┄";
-progressbar_look = "─⊙ ";
-#progressbar_look = "─░─";
-progressbar_elapsed_color = 74;
-#progressbar_elapsed_color = 24; # muted
+      lines_scrolled = 8;
+      mouse_list_scroll_whole_page = true;
+      mouse_support = true;
 
-colors_enabled = true;
+      visualizer_output_name = "Visualizer feed";
+      visualizer_type = "wave_filled"; # wave, wave_filled, spectrum, ellipse
+      visualizer_in_stereo = "yes"; # yes if fifo set to 44100:16:2
+      visualizer_autoscale = true;
+      visualizer_spectrum_smooth_look = true;
+      # visualizer_color = "25, 38, 10";
+      visualizer_color = "25, 38, 104";
+      visualizer_look = "+|";
+      # visualizer_look = "●▮";
 
-playlist_disable_highlight_delay = 5; #0 to disable fade
-playlist_show_remaining_time = true; #header time remaining
-playlist_shorten_total_times = true;
-playlist_separate_albums = false; # add separator lines between albums in playlist view
-ask_before_clearing_playlists = true;
+      ncmpcpp_directory = "~/.local/share/ncmpcpp";
 
-search_engine_display_mode = "columns";
-browser_display_mode = "columns";
-playlist_display_mode = "columns";
-playlist_editor_display_mode = "columns";
-# browser_playlist_prefix = " - ";
-# browser_playlist_prefix = " -> ";
-#clock_display_seconds = yes
+      mpd_crossfade_time = 4;
+      mpd_connection_timeout = 5;
+      # mpd_host = "localhost";
+      mpd_host = "localhost";
+      mpd_port = 6600;
 
-lines_scrolled = 8;
-mouse_list_scroll_whole_page = true;
-mouse_support = true;
+      # execute_on_song_change = "~/.darth/scripts/player/songinfo.sh";
+      # execute_on_song_change = "songart";
+      execute_on_player_state_change = "songart";
 
-visualizer_output_name = "Visualizer feed";
-visualizer_type = "wave_filled"; # wave, wave_filled, spectrum, ellipse
-visualizer_in_stereo = "yes"; # yes if fifo set to 44100:16:2
-visualizer_autoscale = true;
-visualizer_spectrum_smooth_look = true;
-# visualizer_color = "25, 38, 10";
-visualizer_color = "25, 38, 104";
-visualizer_look = "+|";
-# visualizer_look = "●▮";
+      ## Selected tracks ##
+      # selected_item_prefix = "->>";
+      # selected_item_prefix = "|>";
+      selected_item_prefix = "+  ";
+      discard_colors_if_item_is_selected = true;
 
-ncmpcpp_directory = "~/.local/share/ncmpcpp";
+      system_encoding = "utf8";
+      regular_expressions = "extended"; # none/basic/extended/perl;
+      generate_win32_compatible_filenames = true; # generated file names eg. with tag editor wont have  \?*:|
 
-mpd_crossfade_time = 4;
-mpd_connection_timeout = 5;
-# mpd_host = "localhost";
-mpd_host = "localhost";
-mpd_port = 6600;
+      allow_for_physical_item_deletion = true;
+      show_hidden_files_in_local_browser = false;
+      #startup_slave_screen = "browser";
 
-# execute_on_song_change = "~/.darth/scripts/player/songinfo.sh";
-# execute_on_song_change = "songart";
-execute_on_player_state_change = "songart";
-
-## Selected tracks ##
-# selected_item_prefix = "->>";
-# selected_item_prefix = "|>";
-selected_item_prefix = "+  ";
-discard_colors_if_item_is_selected = true;
-
-system_encoding = "utf8";
-regular_expressions =  "extended"; # none/basic/extended/perl;
-generate_win32_compatible_filenames = true; # generated file names eg. with tag editor wont have  \?*:|
-
-allow_for_physical_item_deletion = true;
-show_hidden_files_in_local_browser = false;
-#startup_slave_screen = "browser";
-
-};
- };
-  }
+    };
+  };
+}

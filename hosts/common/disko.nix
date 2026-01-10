5eaac76276cbcb6d1e7617b10c82f9cbfc10db90
darth-malu <darth-malu@github.com>
2026-01-10 16:44:47 +0300

    disko update

--- hosts/common/disko.nix
+++ hosts/common/disko.nix
@@ -1,6 +1,7 @@
 # sudo nix run github:nix-community/disko -- --mode zap_create_mount ./disko-config.nix
 {
   disks ? [ "/dev/nvme0n1" ],
+  zpoolName ? "darthPool",
   ...
 }:
 {
@@ -34,7 +35,7 @@
       };
     };
     zpool = {
-      darthPool = {
+      ${zpoolName} = {
         type = "zpool";
         rootFsOptions = {
           # https://wiki.archlinux.org/title/Install_Arch_Linux_on_ZFS
@@ -43,6 +44,8 @@
           compression = "zstd";
           mountpoint = "none";
           xattr = "sa";
+          encryption = "on";
+          keyFormat = "passphrase";
         };
         options.ashift = "12";
 
@@ -54,24 +57,50 @@
           "root" = {
             type = "zfs_fs";
             mountpoint = "/";
-            options."com.sun:auto-snapshot" = "false";
+            options = {
+              mountpoint = "legacy";
+              "com.sun:auto-snapshot" = "false";
+            };
             # postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^zroot/local/root@blank$' || zfs snapshot zroot/local/root@blank";
           };
           "root/home" = {
             type = "zfs_fs";
             mountpoint = "/home";
             # Used by services.zfs.autoSnapshot options.
-            options."com.sun:auto-snapshot" = "false";
+            options = {
+              mountpoint = "legacy";
+              "com.sun:auto-snapshot" = "false";
+            };
           };
           "root/nix" = {
             type = "zfs_fs";
             mountpoint = "/nix";
-            options."com.sun:auto-snapshot" = "false";
+            options = {
+              mountpoint = "legacy";
+              "com.sun:auto-snapshot" = "false";
+              compression = "lz4";
+            };
           };
           "root/games" = {
             type = "zfs_fs";
             mountpoint = "/home/games";
-            options."com.sun:auto-snapshot" = "false";
+            options = {
+              mountpoint = "legacy";
+              "com.sun:auto-snapshot" = "false";
+            };
+          };
+          "root/reserved" = {
+            type = "zfs_fs";
+            options = {
+              mountpoint = "none";
+              refreservation = "1G";
+            };
+          };
+          "root/tmp" = {
+            type = "zfs_fs";
+            options = {
+              mountpoint = "/tmp";
+            };
           };
           "root/swap" = {
             type = "zfs_volume";

28e197a8ecdcc108169dd4d5064aa28add87d1bc
darth-malu <darth-malu@github.com>
2026-02-01 23:32:57 +0300

    ++

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -25,7 +25,7 @@
         "nvidia-settings"
       ];
     permittedInsecurePackages = [
-      "broadcom-sta-6.30.223.271-59-6.12.67"
+      "broadcom-sta-6.30.223.271-59-6.12.68"
       "libxml2-2.13.8" # for cisco?
     ];
   };

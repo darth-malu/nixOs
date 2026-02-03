6225e5e709bb3c0f13a5203a9545261948633beb
darth-malu <darth-malu@github.com>
2026-02-03 04:26:28 +0300

    ++

--- modules/home/programs/hyprland/hyprland-helper/quickshell.nix
+++ modules/home/programs/hyprland/hyprland-helper/quickshell.nix
@@ -7,7 +7,7 @@
   programs.quickshell = {
     enable = true;
     package = inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.default; # OR use stable
-    activeConfig = "$HOME/Shibuya/modules/home/programs/hyprland/hyprland-helper/quickshell"; # if null use $XDG_CONFIG_HOME/quickshell::
+    # activeConfig = "$HOME/Shibuya/modules/home/programs/hyprland/hyprland-helper/quickshell"; # if null use $XDG_CONFIG_HOME/quickshell::
     systemd = {
       enable = true;
       target = "hyprland-session.target"; # config.wayland.systemd.target:: (graphical-session-target) # HACK:

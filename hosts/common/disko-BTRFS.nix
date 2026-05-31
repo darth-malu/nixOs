9c269fa357423bbc284fb8f45d4350a18a5537bd
darth-malu <darth-malu@github.com>
2026-05-31 20:31:18 +0300

    disko BTRFS

--- hosts/common/disko-BTRFS.nix
+++ hosts/common/disko-BTRFS.nix
@@ -8,11 +8,62 @@
   config,
   ...
 }:
+let
+  rootCommon = {
+    size = "100%";
+    content = {
+      type = "btrfs";
+      extraArgs = [ "-f" ]; # Override existing partition
+      # Subvolumes must set a mountpoint in order to be mounted,
+      # unless their parent is mounted
+      subvolumes = {
+        # Subvolume name is different from mountpoint
+        "/rootfs" = {
+          mountpoint = "/";
+        };
+        # Subvolume name is the same as the mountpoint
+        "/home" = {
+          mountOptions = [ "compress=zstd" ];
+          mountpoint = "/home";
+        };
+        # Sub(sub)volume doesn't need a mountpoint as its parent is mounted
+        "/home/malu" = { };
+        # Parent is not mounted so the mountpoint must be set
+        "/nix" = {
+          mountOptions = [
+            "compress=zstd"
+            "noatime"
+          ];
+          mountpoint = "/nix";
+        };
+        # This subvolume will be created but not mounted
+        # "/test" = { };
+        "/swap" = {
+          mountpoint = "/.swapvol";
+          swap = {
+            swapfile.size = "20M";
+            swapfile2.size = "20M";
+            swapfile2.path = "rel-path";
+          };
+        };
+      };
+
+      mountpoint = "/partition-root";
+      swap = {
+        swapfile = {
+          size = "20M";
+        };
+        swapfile1 = {
+          size = "20M";
+        };
+      };
+    };
+  };
+in
 {
   disko.devices =
     if config.networking.hostName == "carthage" then
       {
-
         disk = {
           samsungNVME = {
             device = builtins.elemAt disks 0;
@@ -21,8 +72,11 @@
               type = "gpt";
               partitions = {
                 ESP = {
+                  name = "ESP";
                   type = "EF00";
-                  size = "500M";
+                  size = "200M";
+                  # start = "1MiB";
+                  # end = "200MiB";
                   content = {
                     type = "filesystem";
                     format = "vfat";
@@ -30,104 +84,7 @@
                     mountOptions = [ "umask=0077" ];
                   };
                 };
-                zfs = {
-                  size = "100%";
-                  content = {
-                    type = "zfs";
-                    pool = "darthPool";
-                  };
-                };
-              };
-            };
-          };
-        };
-        zpool = {
-          darthPool = {
-            type = "zpool";
-            rootFsOptions = {
-              # https://wiki.archlinux.org/title/Install_Arch_Linux_on_ZFS
-              acltype = "posixacl";
-              atime = "off";
-              compression = "zstd";
-              mountpoint = "none";
-              xattr = "sa";
-              # encryption = "on";
-              # keyFormat = "passphrase";  #TODO: next time
-            };
-            options.ashift = "12";
-
-            datasets = {
-              "core" = {
-                type = "zfs_fs";
-                options.mountpoint = "none";
-              };
-              "core/root" = {
-                type = "zfs_fs";
-                mountpoint = "/";
-                options = {
-                  mountpoint = "legacy";
-                  "com.sun:auto-snapshot" = "false";
-                };
-                # postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^zroot/local/root@blank$' || zfs snapshot zroot/local/root@blank";
-              };
-              "core/nix" = {
-                type = "zfs_fs";
-                mountpoint = "/nix";
-                options = {
-                  mountpoint = "legacy";
-                  "com.sun:auto-snapshot" = "false";
-                  compression = "lz4";
-                };
-              };
-              "core/reserved" = {
-                type = "zfs_fs";
-                options = {
-                  mountpoint = "none";
-                  refreservation = "1G";
-                };
-              };
-              "core/tmp" = {
-                type = "zfs_fs";
-                options = {
-                  mountpoint = "/tmp";
-                };
-              };
-              # "core/swap" = {
-              #   type = "zfs_volume";
-              #   size = "4G";
-              #   content = {
-              #     type = "swap";
-              #   };
-              #   options = {
-              #     volblocksize = "4096";
-              #     compression = "zle";
-              #     logbias = "throughput";
-              #     sync = "always";
-              #     primarycache = "metadata";
-              #     secondarycache = "none";
-              #     "com.sun:auto-snapshot" = "false";
-              #   };
-              # };
-              "extra" = {
-                type = "zfs_fs";
-                options.mountpoint = "none";
-              };
-              "extra/home" = {
-                type = "zfs_fs";
-                mountpoint = "/home";
-                # Used by services.zfs.autoSnapshot options.
-                options = {
-                  mountpoint = "legacy";
-                  "com.sun:auto-snapshot" = "false";
-                };
-              };
-              "extra/games" = {
-                type = "zfs_fs";
-                mountpoint = "/games";
-                options = {
-                  mountpoint = "legacy";
-                  "com.sun:auto-snapshot" = "false";
-                };
+                root = rootCommon;
               };
             };
           };
@@ -143,73 +100,17 @@
               type = "gpt";
               partitions = {
                 ESP = {
+                  name = "ESP";
                   type = "EF00";
-                  size = "100M";
+                  size = "200M";
                   content = {
                     type = "filesystem";
                     format = "vfat";
                     mountpoint = "/boot";
-                    mountOptions = [ "umask = 0077" ];
-                  };
-                  zfs = {
-                    size = "100%";
-                    content = {
-                      type = "zfs";
-                      pool = "darthPool";
-                    };
-                  };
-                  zpool = {
-                    darthPool = {
-                      type = "zpool";
-                      rootFsOptions = {
-                        acltype = "posixacl";
-                        atime = "off";
-                        compression = "zstd";
-                        mountpoint = "none";
-                        xattr = "sa";
-                      };
-                      options.ashift = "12";
-                      datasets = {
-                        "core" = {
-                          type = "zfs_fs";
-                          options.mountpoint = "none";
-                        };
-                        "core/root" = {
-                          type = "zfs_fs";
-                          mountpoint = "/";
-                          options = {
-                            mountpoint = "legacy";
-                            "com.sun:auto-snapshot" = "false";
-                          };
-                        };
-                        "core/home" = {
-                          type = "zfs_fs";
-                          mountpoint = "/home";
-                          options = {
-                            mountpoint = "legacy";
-                            "com.sun:auto-snapshot" = "false";
-                          };
-                        };
-                        # "core/swap" = {
-                        #   type = "zfs_volume";
-                        #   size = "4G";
-                        #   content = {
-                        #     type = "swap";
-                        #   };
-                        #   options = {
-                        #     volblocksize = "4096";
-                        #     compression = "zle";
-                        #     logbias = "throughput";
-                        #     sync = "always";
-                        #     primarycache = "metadata";
-                        #     secondarycache = "none";
-                        #     "com.sun:auto-snapshot" = "false";
-                        #   };
-                        # };
-                      };
-                    };
+                    mountOptions = [ "umask=0077" ];
                   };
                 };
+                root = rootCommon;
               };
             };
           };

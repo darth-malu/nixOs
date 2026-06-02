7f98632557970f12f94281a36242c35e3198e1a1
darth-malu <darth-malu@github.com>
2026-06-02 12:43:39 +0300

    -+

--- assets/Lean/disko.sh
+++ assets/Lean/disko.sh
@@ -2,8 +2,7 @@
 #generate config optionally
 # sudo nixos-generate-config --root /mnt --no-filesystems
 #
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount /run/media/nixos/Ventoy/Shibuya/hosts/common/disko-BTRFS-LUKS.nix
-# sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount /run/media/nixos/Ventoy/Shibuya/hosts/common/disko-ZFS.nix
+# sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount /run/media/nixos/Ventoy/Shibuya/hosts/common/disko-BTRFS-LUKS.nix
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount /run/media/nixos/Ventoy/Jaja/Shibuya/assets/Lean/disko.sh
 
 sudo nixos-install --flake /run/media/nixos/Ventoy/Shibuya#tangier #Install
-

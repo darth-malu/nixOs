b22952b7ed7f21290c2c6e61b3b058b37fb111b7
darth-malu <darth-malu@github.com>
2026-06-04 13:12:25 +0300

    tangier: enable intel_pstate=active, powersave governor, thermald, nvidia pm

--- hosts/tangier/default.nix
+++ hosts/tangier/default.nix
@@ -12,10 +12,14 @@
 
   networking.hostName = "tangier";
 
+  boot.kernelParams = [ "intel_pstate=active" ];
+
   powerManagement = {
     enable = true;
-    cpuFreqGovernor = lib.mkDefault "ondemand"; # 'ondemand', 'perfomance', 'powersave'
+    cpuFreqGovernor = lib.mkDefault "powersave"; # 'ondemand', 'perfomance', 'powersave'
   };
+
+  services.thermald.enable = true;
 
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }

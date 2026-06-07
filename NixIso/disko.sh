0610957265f2d99c311f66f1f5b93c2d1f91d1d0
darth-malu <darth-malu@github.com>
2026-06-07 14:07:49 +0300

    new disko

--- NixIso/disko.sh
+++ NixIso/disko.sh
@@ -9,14 +9,20 @@ sudo nix --experimental-features "nix-command flakes" run github:nix-community/d
 
 sudo mount -o subvol=/swap /dev/mapper/GoodLuks /mnt/.swapvol
 
-sudo truncate -s 0 /mnt/.swapvol/swapfile
+$ sudo btrfs filesystem mkswapfile --size 20g /mnt/.swapvol/swapfile
 
-sudo chattr +C /mnt/.swapvol/swapfile
+$ sudo swapon /mnt/.swapvol/swapfile
 
-sudo fallocate -l 26G /mnt/.swapvol/swapfile
+# sudo truncate -s 0 /mnt/.swapvol/swapfile
 
-sudo chmod 600 /mnt/.swapvol/swapfile
+# sudo chattr +C /mnt/.swapvol/swapfile
 
-sudo mkswap /mnt/.swapvol/swapfile
+# sudo fallocate -l 26G /mnt/.swapvol/swapfile
 
-sudo swapon /mnt/.swapvol/swapfile
+# sudo chmod 600 /mnt/.swapvol/swapfile
+
+# sudo mkswap /mnt/.swapvol/swapfile
+
+# sudo swapon /mnt/.swapvol/swapfile
+
+# Use Btrfs Swapfile instead

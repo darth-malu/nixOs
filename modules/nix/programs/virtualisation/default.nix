c9e687099124f15c15f5fe7a6df4e7f8a78514f6
darth-malu <darth-malu@github.com>
2026-06-14 20:49:15 +0300

    Vmware only

--- modules/nix/programs/virtualisation/default.nix
+++ modules/nix/programs/virtualisation/default.nix
@@ -24,9 +24,9 @@
     };
     waydroid.enable = false;
     virtualbox = {
-      guest.enable = true;
+      guest.enable = false;
       host = {
-        enable = true;
+        enable = false;
         enableExtensionPack = false; # usb2/3 forward to guests #NOTE: frequent recompilation with host extensions
         # enableKvm = true; # better compat with linux kernel versions
         # addNetworkInterface = true; # true:: -vboxnet0

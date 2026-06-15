51f8b72424695a7fa6a493c6c6b30505be34b9eb
darth-malu <darth-malu@github.com>
2026-06-15 15:45:42 +0300

    SUnshine

--- modules/nix/programs/gaming/sunshine+moonlight.nix
+++ modules/nix/programs/gaming/sunshine+moonlight.nix
@@ -1,9 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ pkgs, ... }:
 
 {
   environment.systemPackages = with pkgs; [
@@ -20,5 +15,32 @@
       gamepad = "x360";
       # port = 47989;
     };
+  };
+
+  # Enables the uinput kernel module and creates the uinput group
+  hardware.uinput.enable = true;
+
+  # Add your user to the uinput group (replace "your-username" with your actual username)
+
+  users.users.your-username.extraGroups = [ "malu" ];
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [
+      47984
+      47989
+      47990
+      48010
+    ];
+    allowedUDPPortRanges = [
+      {
+        from = 47998;
+        to = 48000;
+      }
+      {
+        from = 8000;
+        to = 8010;
+      }
+    ];
   };
 }

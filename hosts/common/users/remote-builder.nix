cff6eec9433d98d7e21838d9e9ddcf93264ed71e
darth-malu <darth-malu@github.com>
2026-06-19 13:25:21 +0300

    New Remote builder settings

--- hosts/common/users/remote-builder.nix
+++ hosts/common/users/remote-builder.nix
@@ -1,10 +1,15 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
   # client will use this account on the remote
   # ENable distributed builds on client
-  # users.users.remotebuild = lib.mkIf (config.networking.hostName == "carthage") {
-  users.users.remotebuild = {
+  users.users.remotebuild = lib.mkIf (config.networking.hostName == "carthage") {
+    # users.users.remotebuild = {
     isNormalUser = true;
     createHome = false;
     group = "remotebuild";
@@ -25,10 +30,38 @@
       ];
       min-free = 10 * 1024 * 1024;
       max-free = 200 * 1024 * 1024;
-
       max-jobs = "auto";
       cores = 0;
     };
+  };
+
+  nix = {
+    distributedBuilds = lib.mkIf (config.networking.hostName != "carthage") true;
+    buildMachines = lib.mkIf (config.networking.hostName != "carthage") [
+      {
+        # makes it so i dont have to use --builders "ssh://myuser@builder <other builder specification>"
+        sshUser = "remotebuild"; # NOTE special user that cant be sudo'd into
+        sshKey = "/home/malu/.ssh/id_ed25519"; # path to remote key on client (tangier)
+        hostName = "192.168.1.4"; # Replace by IP address, or add a ProxyCommand, see `man ssh_config` for full docs.
+        protocol = "ssh-ng"; # ssh:: ssh-ng ( ssh next generation)
+        maxJobs = 3; # 1::
+        speedFactor = 2; # The relative speed of this builder. This is an arbitrary integer that indicates the speed of this builder, relative to other builders. Higher is faster.
+        # system = pkgs.stdenv.hostPlatform.system;
+        system = "x86_64-linux";
+        supportedFeatures = [
+          "nixos-test"
+          "benchmark"
+          "big-parallel"
+          # "kvm"
+        ];
+        mandatoryFeatures = [ ];
+      }
+    ];
+    extraOptions = lib.mkIf (config.networking.hostName != "carthage") ''
+      builders-use-substitutes = true
+      keep-outputs = true;
+      keep-derivations = true;
+    ''; # optional, useful when the builder has a faster internet connection than yours
   };
 
   systemd.services.nix-daemon.serviceConfig = {

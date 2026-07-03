f8929de593a5b5bfbfd58a38173050e2f4f22541
darth-malu <justinmalu@gmail.com>
2026-07-04 02:28:15 +0300

    refactor - move to nix from home-manager [Begins]

--- modules/home/programs/pkgs.nix
+++ modules/home/programs/pkgs.nix
@@ -1,134 +1,99 @@
 {
-  osConfig,
   pkgs,
-  pkgs-stable,
-  inputs,
-  lib,
+  config,
   ...
 }:
 
 {
-  imports = [
-    ./programs+options.nix
-    ./developerPkgs.nix
-  ];
+  programs = {
+    home-manager.enable = true; # Let Home Manager install and manage itself.
 
-  home.packages =
-    with pkgs;
-    [
-      # inputs.vermilion.packages.${pkgs.stdenv.hostPlatform.system}.default
-    ]
-    ++ [
-      grim
-      imagemagick # screenshot
-      zenity
-      # sound-theme-freedesktop # free sounds
-      libcanberra-gtk3
-      kdePackages.kasts
-      kdePackages.ksudoku
-      kdePackages.kservice
-      kdePackages.konsole
-      # KIO is a library that provides file manipulation and copying, remote file access, thumbnail previews, the Trash, the Places panel, and various other common file-related user interface components.
-      kdePackages.kio-fuse # FUSE Interface for KIO
-      kdePackages.kio-extras # Additional components to increase the functionality of KIO
-      kdePackages.kio-admin # Manage files as administrator using the admin:// KIO protocol
-      # kdePackages.kio-gdrive
-      kdePackages.qtwayland # allows Qt-based applications to run on the Wayland display protocol
-    ]
-    ++ [
-      # pastel # color generator
-      # qalculate-qt # #FIXME:for rofi?
-      # yaru-theme
-      # aria2#NOTE: learn
-      # libsecret # TODO use
-      ddrescue
-      testdisk
-      trash-cli
-      wev # NOTE use ./ascii
-      jdupes
-      cryptsetup
-      pass-wayland
-    ]
-    ++ [
-      # testing for yazi #
-      zip # Compressor/archiver for creating and modifying zipfiles.
-      unzip # Extraction utility for archives compressed in .zip format
-      xz # General-purpose data compression software, successor of LZMA
-      p7zip # New p7zip fork with additional codecs and improvements (forked from https://sourceforge.net/projec…
-      rar # also has unrar
-    ]
-    ++ [
-      # Fediverse
-      telegram-desktop
-      # signal-desktop
-      # mastodon # TODO: self host only? no desktop
-      cinny-desktop
-      # rocketchat-desktop
-      # element-desktop
-    ]
-    ++ [
-      # SOUND
-      cantata # old af
-      # clapper
-      # kando
-      lollypop # cantata better
-      # qqmusic
-      # spotify-cli-linux
-      # tenacity
-      # wf-recorder
-      # ymuse # rudimentary
-      # youtube-tui # https://siriusmart.github.io/youtube-tui/
-      audacity
-      blanket
-      qbittorrent
-      gpu-screen-recorder-gtk
-      alarm-clock-applet
-      spotify
-      spotube
-      stremio-linux-shell
-      sway-audio-idle-inhibit
-    ]
-    ++ [
-      # komikku # broken
-      mangal
-      ani-cli
-      # kazumi # watch anime online with danmaku support
-      # miru # issue launching -kde
-      #syncyomi - sync tachiyomi progress across devices
-    ]
-    ++ [
-      # coppwr
-      # helvum
-      # kdePackages.mlt
-      # ffmpeg-full
-      handbrake # FIXME lag on open
-      # obs-cli
-      obs-studio
-    ]
-    ++ [
-      bluemail
-      inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
-    ]
-    ++ [
-      # Productivity Tools
-      # libreoffice-qt-still
-      # libreoffice-still
-      onlyoffice-desktopeditors
-      # wordbook
-      # wpsoffice
-    ]
-    ++ [
-      # CreativeSPACE 📽
-      blender
-      darktable # Artist
-      digikam # Librarian
-      gimp
-      inkscape-with-extensions
-      krita
-      kdePackages.kdenlive
-      # figma-linux
-      # figma-agent  #NOTE listens on 127.0.0.1:44950
-      pkgs-stable.davinci-resolve
-      exiftool
-    ];
+    btop = import ./misc/btop.nix;
+
+    comodoro.enable = false;
+
+    fastfetch = import ./misc/fastfetch.nix; # ALso have hyprsysteminfo
+
+    fd = import ./misc/fd.nix;
+
+    freetube = import ./misc/freetube.nix;
+
+    fzf = import ./misc/fzf.nix;
+
+    # java.enable = true;
+
+    # lazygit = import ./misc/lazygit.nix;
+
+    lsd = import ./misc/lsd.nix;
+
+    # nix-index.enable = false;
+
+    # nh.enable = true;
+
+    pandoc.enable = true;
+
+    ripgrep = {
+      enable = true;
+      arguments = [
+        # https://github.com/BurntSushi/ripgrep/blob/master/GUIDE.md#configuration-file
+        "--max-columns-preview"
+        "--colors=line:style:bold"
+      ];
+    };
+
+    # spotify-player = import ./misc/spotify-player.nix;
+
+    zathura = import ./misc/zathura.nix;
+
+    zoxide = {
+      enable = true;
+      enableBashIntegration = config.programs.bash.enable;
+      options = [ "--cmd cd" ];
+    };
+
+    bat = {
+      enable = true;
+      extraPackages = with pkgs.bat-extras; [
+        batdiff
+        batman
+        batgrep
+        batwatch
+      ];
+      syntaxes = {
+        gleam = {
+          src = pkgs.fetchFromGitHub {
+            owner = "molnarmark";
+            repo = "sublime-gleam";
+            rev = "2e761cdb1a87539d827987f997a20a35efd68aa9";
+            hash = "sha256-Zj2DKTcO1t9g18qsNKtpHKElbRSc9nBRE2QBzRn9+qs=";
+          };
+          file = "syntax/gleam.sublime-syntax";
+        };
+      };
+
+      themes = {
+        dracula = {
+          src = pkgs.fetchFromGitHub {
+            owner = "dracula";
+            repo = "sublime"; # Bat uses sublime syntax for its themes
+            rev = "26c57ec282abcaa76e57e055f38432bd827ac34e";
+            sha256 = "019hfl4zbn4vm4154hh3bwk6hm7bdxbr1hdww83nabxwjn99ndhv";
+          };
+          file = "Dracula.tmTheme";
+        };
+      };
+    };
+
+    antigravity-cli = {
+      enable = true;
+      defaultModel = "gemini-2.5-flash";
+      # context
+      settings = {
+        theme = "Default";
+        vimMode = true;
+        preferredEditor = "vim";
+        autoAccept = true;
+      };
+    };
+  };
 }
--- modules/nix/programs/pkgs.nix
+++ modules/nix/programs/pkgs.nix
@@ -1,114 +1,238 @@
-{ pkgs, ... }:
 {
-  environment = {
-    # List of packages installed in system profile. To search, run: $ nix search wget
-    systemPackages =
-      with pkgs;
-      [
-        libnotify # notify-send
-        wget
-        nvd
-        app2unit
-        #cpufrequtils
-        lshw
-        e2fsprogs # chattr etc
-        efibootmgr
-        curl
-        # dash
-        procs
-        # stacer
-        fio
-        killall
-        nix-prefetch-git # nix-prefetch-scripts #includes git prefetch
-        pipewire
-        pwvucontrol
-        playerctl
-        lsof # list open files/ports**
-        usbutils # lsusb, usb-devices, usb-view(optional gui)
-        jmtpfs # mtp drivers - # TODO check if mtp-probe error is from this and how to mitigate
-        pciutils # lspci
-        util-linux # fdisk, findmnt, kill, chsh, dmesg, eject, fstrim, hwclock, more**
-        kitty
-        # emacs
-        # bc
-        wl-clipboard # rust wl-clipboard better?
-        dotool # test if working
-        easyeffects
-        mpc
-        discord
-        pv
-        # ff2mpv-rust
-        ff2mpv
-        # gsmartcontrol
-        # iotop #basic
-        # perfomance monitoring
-        # qdiskinfo
-        # squirreldisk #kinda nice
-        # superfile # kinda cool but dont need
-        # testdisk-qt
-        # utilities
-        # warp-terminal # insane bloat
-        # win-disk-writer
-        # testdisk # also installs photorec
-        # duc
-        # dust
-        duf
-        ncdu
-        file # need for yazi mimedetection etc.
-        iftop # TODO seems powerful investigate more
-        inxi
-        iotop-c
-        lm_sensors
-        lsof # list open files/ports**
-        mission-center
-        nethogs
-        pciutils # lspci
-        usbutils # lsusb, usb-devices, usb-view(optional gui)
-        util-linux # fdisk, findmnt, kill, chsh, dmesg, eject, fstrim, hwclock
-        xdg-utils
-        psmisc # fuser,killall?
-        testdisk
-        testdisk-qt
-        extundelete
-        ripgrep-all
-        dotool
-        httrack
-        modem-manager-gui
-        modemmanager
-        whatsapp-electron
-        ventoy-full # usb imager #woeusb
-        nautilus
-        ffmpeg
-        google-chrome
-        # firefox
-        # ungoogled-chromium
-        vulkan-tools
-        libva-utils
-        zenith
-        powershell
-        localsend
-      ]
-      ++ lib.optionals config.programs.hyprland.enable [
-        hyprpicker
-        # hyprpolkitagent
-        libappindicator-gtk3 # needed for discord icon
-        # libsecret # secrets lul
-        grimblast
-        # file-roller
-        # inputs.hyprswitch.packages.x86_64-linux.default
-        # yelp
-        slurp
-        sushi
-        viewnior
-        kdePackages.kalk
-        kdePackages.dolphin
-        hyprsysteminfo
-        hyprshutdown
-        hyprpwcenter
-        hyprland-autoname-workspaces
-      ]
-      ++ lib.optionals (config.networking.hostName == "tangier") [
-        brightnessctl
-      ];
-  };
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+{
+  imports = [
+    ./developerPkgs.nix
+  ];
+  # List of packages installed in system profile. To search, run: $ nix search wget
+  environment.systemPackages =
+    with pkgs;
+    [
+      libnotify # notify-send
+      wget
+      nvd
+      app2unit
+      #cpufrequtils
+      lshw
+      e2fsprogs # chattr etc
+      efibootmgr
+      curl
+      # dash
+      procs
+      # stacer
+      fio
+      killall
+      nix-prefetch-git # nix-prefetch-scripts #includes git prefetch
+      pipewire
+      pwvucontrol
+      playerctl
+      lsof # list open files/ports**
+      usbutils # lsusb, usb-devices, usb-view(optional gui)
+      jmtpfs # mtp drivers - # TODO check if mtp-probe error is from this and how to mitigate
+      pciutils # lspci
+      util-linux # fdisk, findmnt, kill, chsh, dmesg, eject, fstrim, hwclock, more**
+      kitty
+      # emacs
+      # bc
+      wl-clipboard # rust wl-clipboard better?
+      dotool # test if working
+      easyeffects
+      mpc
+      discord
+      pv
+      # ff2mpv-rust
+      ff2mpv
+      # gsmartcontrol
+      # iotop #basic
+      # perfomance monitoring
+      # qdiskinfo
+      # squirreldisk #kinda nice
+      # superfile # kinda cool but dont need
+      # testdisk-qt
+      # utilities
+      # warp-terminal # insane bloat
+      # win-disk-writer
+      # testdisk # also installs photorec
+      # duc
+      # dust
+      duf
+      ncdu
+      file # need for yazi mimedetection etc.
+      iftop # TODO seems powerful investigate more
+      inxi
+      iotop-c
+      lm_sensors
+      lsof # list open files/ports**
+      mission-center
+      nethogs
+      pciutils # lspci
+      usbutils # lsusb, usb-devices, usb-view(optional gui)
+      util-linux # fdisk, findmnt, kill, chsh, dmesg, eject, fstrim, hwclock
+      xdg-utils
+      psmisc # fuser,killall?
+      testdisk
+      testdisk-qt
+      extundelete
+      ripgrep-all
+      dotool
+      httrack
+      modem-manager-gui
+      modemmanager
+      whatsapp-electron
+      ventoy-full # usb imager #woeusb
+      nautilus
+      ffmpeg
+      google-chrome
+      # firefox
+      # ungoogled-chromium
+      vulkan-tools
+      libva-utils
+      zenith
+      powershell
+      localsend
+    ]
+    ++ pkgs.lib.optionals (config.hypr.enable) [
+      hyprpicker
+      # hyprpolkitagent
+      libappindicator-gtk3 # needed for discord icon
+      # libsecret # secrets lul
+      grimblast
+      # file-roller
+      # inputs.hyprswitch.packages.x86_64-linux.default
+      # yelp
+      slurp
+      sushi
+      viewnior
+      kdePackages.kalk
+      kdePackages.dolphin
+      hyprsysteminfo
+      hyprshutdown
+      hyprpwcenter
+      hyprland-autoname-workspaces
+    ]
+    ++ pkgs.lib.optionals (config.networking.hostName == "tangier") [
+      brightnessctl
+    ]
+    ++
+    [
+      # inputs.vermilion.packages.${pkgs.stdenv.hostPlatform.system}.default
+    ]
+    ++ [
+      grim
+      imagemagick # screenshot
+      zenity
+      # sound-theme-freedesktop # free sounds
+      libcanberra-gtk3
+      kdePackages.kasts
+      kdePackages.ksudoku
+      kdePackages.kservice
+      kdePackages.konsole
+      # KIO is a library that provides file manipulation and copying, remote file access, thumbnail previews, the Trash, the Places panel, and various other common file-related user interface components.
+      kdePackages.kio-fuse # FUSE Interface for KIO
+      kdePackages.kio-extras # Additional components to increase the functionality of KIO
+      kdePackages.kio-admin # Manage files as administrator using the admin:// KIO protocol
+      # kdePackages.kio-gdrive
+      kdePackages.qtwayland # allows Qt-based applications to run on the Wayland display protocol
+    ]
+    ++ [
+      # pastel # color generator
+      # qalculate-qt # #FIXME:for rofi?
+      # yaru-theme
+      # aria2#NOTE: learn
+      # libsecret # TODO use
+      ddrescue
+      testdisk
+      trash-cli
+      wev # NOTE use ./ascii
+      jdupes
+      cryptsetup
+      pass-wayland
+    ]
+    ++ [
+      # testing for yazi #
+      zip # Compressor/archiver for creating and modifying zipfiles.
+      unzip # Extraction utility for archives compressed in .zip format
+      xz # General-purpose data compression software, successor of LZMA
+      p7zip # New p7zip fork with additional codecs and improvements (forked from https://sourceforge.net/projec…
+      rar # also has unrar
+    ]
+    ++ [
+      # Fediverse
+      telegram-desktop
+      # signal-desktop
+      # mastodon # TODO: self host only? no desktop
+      cinny-desktop
+      # rocketchat-desktop
+      # element-desktop
+    ]
+    ++ [
+      # SOUND
+      cantata # old af
+      # clapper
+      # kando
+      lollypop # cantata better
+      # qqmusic
+      # spotify-cli-linux
+      # tenacity
+      # wf-recorder
+      # ymuse # rudimentary
+      # youtube-tui # https://siriusmart.github.io/youtube-tui/
+      audacity
+      blanket
+      qbittorrent
+      gpu-screen-recorder-gtk
+      alarm-clock-applet
+      spotify
+      spotube
+      stremio-linux-shell
+      sway-audio-idle-inhibit
+    ]
+    ++ [
+      # komikku # broken
+      mangal
+      ani-cli
+      # kazumi # watch anime online with danmaku support
+      # miru # issue launching -kde
+      #syncyomi - sync tachiyomi progress across devices
+    ]
+    ++ [
+      # coppwr
+      # helvum
+      # kdePackages.mlt
+      # ffmpeg-full
+      handbrake # FIXME lag on open
+      # obs-cli
+      obs-studio
+    ]
+    ++ [
+      bluemail
+      inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
+    ]
+    ++ [
+      # Productivity Tools
+      # libreoffice-qt-still
+      # libreoffice-still
+      onlyoffice-desktopeditors
+      # wordbook
+      # wpsoffice
+    ]
+    ++ [
+      # CreativeSPACE 📽
+      blender
+      darktable # Artist
+      digikam # Librarian
+      gimp
+      inkscape-with-extensions
+      krita
+      kdePackages.kdenlive
+      # figma-linux
+      # figma-agent  #NOTE listens on 127.0.0.1:44950
+      davinci-resolve
+      exiftool
+    ];
+  ;
 }

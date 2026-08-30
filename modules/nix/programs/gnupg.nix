6f12313783a344b6a1ee0ee822ef284b10ecd00d
darth-malu <justinmalu@gmail.com>
2026-08-30 17:52:49 +0300

    sshSupport

--- modules/nix/programs/gnupg.nix
+++ modules/nix/programs/gnupg.nix
@@ -5,7 +5,8 @@
     enable = true;
     enableBrowserSocket = true;
     pinentryPackage = pkgs.pinentry-curses; # TODO see implication of curses vs emacs
-    enableSSHSupport = true; # .Also sets SSH_AUTH_SOCK environment variable correctly. Disables socket and activate once per user session
+    enableSSHSupport = false; # .Also sets SSH_AUTH_SOCK environment variable correctly. Disables socket and activate once per user session
+    # NOTE: prompt on every git opp sometimes weird
   };
 
   security.pam.services.login.gnupg = {
